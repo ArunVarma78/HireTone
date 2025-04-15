57f00d624c48d5a0435de9b4712a22f969f228e1
ArunVarma78 <arunvarma4873@gmail.com>
2025-04-15 18:21:17 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HireTone",
+  description:
+    "HireTone is a full-stack, real-time AI-powered voice interview platform designed to simulate human-like conversations for candidate screening. It leverages advanced speech recognition, AI dialogue systems, and real-time audio streaming to conduct, analyze, and score interviews automatically.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
