924fab2e25b42b54729330b9cb071a3414e64b1b
ArunVarma78 <arunvarma4873@gmail.com>
2025-04-16 14:32:03 +0530

    add navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,18 @@
 import { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Rootlayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+
+          <h2 className="text-primary-100">HireTone</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 }
