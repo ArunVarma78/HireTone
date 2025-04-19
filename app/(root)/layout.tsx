705de9942031291d1e4a4608e1150e65a863a53b
ArunVarma78 <arunvarma4873@gmail.com>
2025-04-19 22:38:27 +0530

    Implement Firebase authentication for login and signup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
 import { ReactNode } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
-export default function Rootlayout({ children }: { children: ReactNode }) {
+export default async function Rootlayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) redirect("/sign-in");
+
   return (
     <div className="root-layout">
       <nav>
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,12 @@ import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import { toast } from "sonner";
 import FormField from "./FormField";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 function authFormSchema(type: FormType) {
   return z.object({
@@ -49,16 +55,52 @@ export default function AuthForm({ type }: { type: FormType }) {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        toast.success("Account created successfully!");
+        const { name, email, password } = values;
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
+        toast.success("Account created successfully, Please sign in.");
         router.push("/sign-in");
-        console.log("Sign up", values);
       } else {
+        const { email, password } = values;
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Sing in failed.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
         toast.success("Signed in successfully!");
         router.push("/");
-        console.log("Sign in", values);
       }
     } catch (error) {
       console.log("Error submitting form", error);
